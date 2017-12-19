58fb9dbade1347610578d52250a55382158f294d
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2017-12-19 15:16:08 +0900

    rewrite with class

--- src/thermometer.ino
+++ src/thermometer.ino
@@ -1,102 +1,171 @@
-//  - DHT11 x1
-//    1. input pin
-//  - 7seg (2231B) x1 + 8bit shift register x1 + 2SC1815 x2
-//    1. data pin
-//    2. clock pin
-//    3. latch pin
-//    4. fig.1 pin
-//    5. fig.2 pin
-//  - common
-//    1. Vcc
-//    2. GND
-
+#include <TimerOne.h>
+#include <SoftwareSerial.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#include <TimerOne.h>
 
-class Digits {
+template<
+  int DATA_PIN,
+  int CLOCK_PIN,
+  int LATCH_PIN,
+  int FIG1_PIN,
+  int FIG2_PIN>
+class Display {
 public:
-  Digits(int data_pin, int clock_pin, int latch_pin, int fig1_pin, int fig2_pin) :
-    data_pin_(data_pin),
-    clock_pin_(clock_pin),
-    latch_pin_(latch_pin),
-    fig1_pin_(fig1_pin),
-    fig2_pin_(fig2_pin) {}
+  static const int DataPin = DATA_PIN;
+  static const int ClockPin = CLOCK_PIN;
+  static const int LatchPin = LATCH_PIN;
+  static const int Fig1Pin = FIG1_PIN;
+  static const int Fig2Pin = FIG2_PIN;
 
-  void init() {
-    pinMode(data_pin_, OUTPUT);
-    pinMode(clock_pin_, OUTPUT);
-    pinMode(latch_pin_, OUTPUT);
+  void setup() {
+    pinMode(DataPin, OUTPUT);
+    pinMode(ClockPin, OUTPUT);
+    pinMode(LatchPin, OUTPUT);
+    pinMode(Fig1Pin, OUTPUT);
+    pinMode(Fig2Pin, OUTPUT);
   }
 
-  void show(int value, bool dot1, bool dot2) {
-    showFig1(value / 10, dot1);
-    showFig2(value % 10, dot2);
-  }
+  void show(int value, byte dps, int interval = 10) const {
+    const byte dp1 = static_cast<byte>((dps << 7) & 0x80);
+    const byte dp2 = static_cast<byte>((dps << 6) & 0x80);
+    const byte fig1 = static_cast<byte>((value >> 8) & 0x0f) | dp1;
+    const byte fig2 = static_cast<byte>(value & 0x0f) | dp2;
 
-  void showFig1(int value, bool dot) {
-    digitalWrite(latch_pin_, LOW);
-    shiftOut(data_pin_, clock_pin_, LSBFIRST, value);
-    digitalWrite(fig2_pin_, LOW);
-    digitalWrite(fig1_pin_, HIGH);
-    digitalWrite(latch_pin_, HIGH);
-  }
-
-  void showFig2(int value, bool dot) {
-    digitalWrite(latch_pin_, LOW);
-    shiftOut(data_pin_, clock_pin_, LSBFIRST, value);
-    digitalWrite(fig1_pin_, LOW);
-    digitalWrite(fig2_pin_, HIGH);
-    digitalWrite(latch_pin_, HIGH);
+    showFigure(fig1, Fig1Pin, Fig2Pin);
+    delay(interval);
+    showFigure(fig2, Fig2Pin, Fig2Pin);
+    delay(interval);
   }
 
 private:
-  const int data_pin_;
-  const int clock_pin_;
-  const int latch_pin_;
-  const int fig1_pin_;
-  const int fig2_pin_;
+  void showFigure(byte value, int onPin, int offPin) const {
+    digitalWrite(LatchPin, LOW);
+    shiftOut(DataPin, ClockPin, LSBFIRST, value);
+    digitalWrite(LatchPin, HIGH);
+    digitalWrite(onPin, HIGH);
+    digitalWrite(offPin, LOW);
+  }
 };
 
-static const int DATA_PIN = 1;
-static const int CLOCK_PIN = 2;
-static const int LATCH_PIN = 3;
-static const int FIG1_PIN = 4;
-static const int FIG2_PIN = 5;
+template<int DHT_PIN, int DHTTYPE>
+class ThermoSensor {
+public:
+  static const int DhtPin = DHT_PIN;
 
-Digits digits(DATA_PIN, CLOCK_PIN, LATCH_PIN, FIG1_PIN, FIG2_PIN);
+  ThermoSensor() : dht_(DHT_PIN, DHTTYPE), humidity_(0), temperature_(0), heat_index_(0) {}
 
-static const int DHTPIN = 8;
-static const int DHTTYPE = DHT11;
+  void setup() {
+    dht_.begin();
 
-DHT dht(DHTPIN, DHTTYPE);
-static volatile int temperature;
-static volatile int humidity;
+    humidity_ = 0;
+    temperature_ = 0;
+    heat_index_ = 0;
+  }
 
-static bool showingTemperature;
+  bool fetch() {
+    float humidity = dht_.readHumidity();
+    float temperature = dht_.readTemperature();
 
-void fetchDHT() {
-  temperature = dht.readTemperature(true);
-  humidity = dht.readTemperature();
+    if(isnan(humidity) || isnan(temperature)) {
+      return false;
+    }
 
-  showingTemperature = !showingTemperature;
+    humidity_ = humidity;
+    temperature_ = temperature_;
+    heat_index_ = dht_.computeHeatIndex(temperature_, humidity_, false);
+
+    return true;
+  }
+
+  float humidity() const { return humidity_; }
+  float temperature() const { return temperature_; }
+  float heat_index() const { return heat_index_; }
+
+private:
+  DHT dht_;
+  float humidity_;
+  float temperature_;
+  float heat_index_;
+};
+
+class Thermometer {
+public:
+  static const int DataPin = 2;
+  static const int ClockPin = 3;
+  static const int LatchPin = 4;
+  static const int Fig1Pin = 5;
+  static const int Fig2Pin = 6;
+  static const int DhtPin = 7;
+  static const int DhtType = DHT11;
+
+  static const unsigned long SamplingInterval = 2000000; // usec
+
+  static void sampling();
+
+  Thermometer() : display_(), thermosensor_() {}
+
+  void setup() {
+    display_.setup();
+    thermosensor_.setup();
+
+    humidity_ = 0;
+    temperature_ = 0;
+    showing_temperature_ = true;
+  }
+
+  void readSerial() {
+  }
+
+  void process() {
+    if(showing_temperature_) {
+      display_.show(temperature_, false);
+    } else {
+      display_.show(humidity_, true);
+    }
+  }
+
+private:
+  Display<
+    DataPin,
+    ClockPin,
+    LatchPin,
+    Fig1Pin,
+    Fig2Pin
+  > display_;
+
+  ThermoSensor<
+    DhtPin,
+    DhtType
+  > thermosensor_;
+
+  float humidity_;
+  float temperature_;
+  bool showing_temperature_;
+};
+
+Thermometer thermometer;
+
+void Thermometer::sampling() {
+  if(thermometer.thermosensor_.fetch()) {
+    thermometer.humidity_ = thermometer.thermosensor_.humidity();
+    thermometer.temperature_ = thermometer.thermosensor_.temperature();
+  }
+
+  thermometer.showing_temperature_ = !thermometer.showing_temperature_;
+}
+
+//----------------------------------------------------------------
+
+void serialEvent() {
+  thermometer.readSerial();
 }
 
 void setup() {
-  temperature = 0;
-  humidity = 0;
-  showingTemperature = true;
-
-  Timer1.initialize(2000000);
-  Timer1.attachInterrupt(fetchDHT);
-
-  digits.init();
+  thermometer.setup();
+  Timer1.initialize(Thermometer::SamplingInterval);
+  Timer1.attachInterrupt(Thermometer::sampling);
 }
 
 void loop() {
-  if(showingTemperature) {
-    digits.show(temperature, false, false);
-  } else {
-    digits.show(humidity, false, true);
-  }
+  thermometer.process();
 }
